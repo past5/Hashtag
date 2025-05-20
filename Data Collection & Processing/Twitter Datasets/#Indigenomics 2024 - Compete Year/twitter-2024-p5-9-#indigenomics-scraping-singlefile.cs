dd9e7cf4530604e6423aa159fe9f27390a8be23b
kIndigenomics <kwaterman@indigenomics.com>
2025-05-19 22:12:24 -0700

    Create twitter-2024-p5-9-#indigenomics-scraping-singlefile.cs

---
+++ Data Collection & Processing/Twitter Datasets/#Indigenomics 2024 - Compete Year/twitter-2024-p5-9-#indigenomics-scraping-singlefile.cs
@@ -0,0 +1,58 @@
+name,username,is_verified,body,media-header,text,date,stats,timeline_item,url
+,,,,,,,,,
+"Carol Anne Hilton, MBA",@Hesquiaht,,"Carol Anne Hilton, MBA@Hesquiaht23 Apr 2024The Indigenomics Institute extends a heartfelt thank you to TELUS, our esteemed Platinum Multi-Year Events Partner, for their unwavering support in championing Indigenous economic empowerment.
+
+Register today!events.indigenomicsinstitute…See you at the conference!#Indigenomics11","Carol Anne Hilton, MBA@Hesquiaht23 Apr 2024","The Indigenomics Institute extends a heartfelt thank you to TELUS, our esteemed Platinum Multi-Year Events Partner, for their unwavering support in championing Indigenous economic empowerment.
+
+Register today!events.indigenomicsinstitute…See you at the conference!#Indigenomics",23 Apr 2024,11,,
+"Carol Anne Hilton, MBA",@Hesquiaht,,"Carol Anne Hilton, MBA@Hesquiaht22 Apr 2024Join a powerful gathering of Indigenous women in business at the Indigenomics SHE Conference,hosted by the Indigenomics Institute in partnership with the National Indigenous Hub for the Women Entrepreneurship Knowledge@wekh_pcfeevents.indigenomicsinstitute…#Indigenomics#WEKH217","Carol Anne Hilton, MBA@Hesquiaht22 Apr 2024","Join a powerful gathering of Indigenous women in business at the Indigenomics SHE Conference,hosted by the Indigenomics Institute in partnership with the National Indigenous Hub for the Women Entrepreneurship Knowledge@wekh_pcfeevents.indigenomicsinstitute…#Indigenomics#WEKH",22 Apr 2024,217,,
+"Carol Anne Hilton, MBA",@Hesquiaht,,"Carol Anne Hilton, MBA@Hesquiaht18 Apr 2024The Indigenomics Institute is excited to welcome Scotia Bank as a Bronze Sponsor for the Indigenomics Design-Indigenous Wealth Dynamics Conference!
+
+We invite you to join us in Saskatoon on June 4th. See you there!
+Learn more:events.indigenomicsinstitute…#Indigenomics#Saskatoon","Carol Anne Hilton, MBA@Hesquiaht18 Apr 2024","The Indigenomics Institute is excited to welcome Scotia Bank as a Bronze Sponsor for the Indigenomics Design-Indigenous Wealth Dynamics Conference!
+
+We invite you to join us in Saskatoon on June 4th. See you there!
+Learn more:events.indigenomicsinstitute…#Indigenomics#Saskatoon",18 Apr 2024,,,
+"Carol Anne Hilton, MBA",@Hesquiaht,,"Carol Anne Hilton, MBA@Hesquiaht16 Apr 2024The Indigenomics Institute welcomes the First Nations Capital and Infrastructure Agency  of Saskatchewan, as a vendor at the upcoming Indigenomics by Design market in Saskatoon on June 4th!
+
+Secure your spot here:events.indigenomicsinstitute…#Indigenomics#Empowerment#Saskatoon2","Carol Anne Hilton, MBA@Hesquiaht16 Apr 2024","The Indigenomics Institute welcomes the First Nations Capital and Infrastructure Agency  of Saskatchewan, as a vendor at the upcoming Indigenomics by Design market in Saskatoon on June 4th!
+
+Secure your spot here:events.indigenomicsinstitute…#Indigenomics#Empowerment#Saskatoon",16 Apr 2024,2,,
+"Carol Anne Hilton, MBA",@Hesquiaht,,"Carol Anne Hilton, MBA@Hesquiaht15 Apr 2024What does#Indigenomicsmean to Indigenomics DESIGN speakers?
+
+Don't miss out on this opportunity to connect, learn, and grow together!
+
+Secure your spot now:events.indigenomicsinstitute…See you there!#IndigenomicsDesign#Saskatoon#Saskatchewan#FinancialWellBeing11","Carol Anne Hilton, MBA@Hesquiaht15 Apr 2024","What does#Indigenomicsmean to Indigenomics DESIGN speakers?
+
+Don't miss out on this opportunity to connect, learn, and grow together!
+
+Secure your spot now:events.indigenomicsinstitute…See you there!#IndigenomicsDesign#Saskatoon#Saskatchewan#FinancialWellBeing",15 Apr 2024,11,,
+Don Richardson,@Don_Richardson,,"Don Richardson@Don_Richardson13 Apr 2024First Nations, Bruce Power and Canada Fight Cancer with Isotopes#MedicalIsotopes#CancerResearch#Collaboration#MedicalIsotopeEcosystem#Indigenomicsminogi.ca/insights/f/first-n…21",Don Richardson@Don_Richardson13 Apr 2024,"First Nations, Bruce Power and Canada Fight Cancer with Isotopes#MedicalIsotopes#CancerResearch#Collaboration#MedicalIsotopeEcosystem#Indigenomicsminogi.ca/insights/f/first-n…",13 Apr 2024,21,,
+"Carol Anne Hilton, MBA",@Hesquiaht,,"Carol Anne Hilton, MBA@Hesquiaht11 Apr 2024As the moon rotates around the earth the countdown clock gets closer to our inaugural virtual 24 hour Indigenous global economic forum! 
+
+Member registration is ready for you here:events.indigenomicsinstitute…#Indigenomics#JoinTheConversation#IndigenomicsNOW#IndigenousVoices","Carol Anne Hilton, MBA@Hesquiaht11 Apr 2024","As the moon rotates around the earth the countdown clock gets closer to our inaugural virtual 24 hour Indigenous global economic forum! 
+
+Member registration is ready for you here:events.indigenomicsinstitute…#Indigenomics#JoinTheConversation#IndigenomicsNOW#IndigenousVoices",11 Apr 2024,,,
+Don Richardson,@Don_Richardson,,Don Richardson@Don_Richardson10 Apr 2024$PHOSFirst Phosphate and Pekuakamiulnuatsh First Nation ink collaboration agreement for phosphate mine and LFP plant project in the Saguenay-Lac-Saint-Jean region of Quebectinyurl.com/29b5c4s3@FirstPhosphate#PHOS#Indigenomics23,Don Richardson@Don_Richardson10 Apr 2024,$PHOSFirst Phosphate and Pekuakamiulnuatsh First Nation ink collaboration agreement for phosphate mine and LFP plant project in the Saguenay-Lac-Saint-Jean region of Quebectinyurl.com/29b5c4s3@FirstPhosphate#PHOS#Indigenomics,10 Apr 2024,23,,
+"Carol Anne Hilton, MBA",@Hesquiaht,,"Carol Anne Hilton, MBA@Hesquiaht9 Apr 2024Indigenomics DESIGN-Indigenous Wealth Dynamics will explore how Indigenous perspectives on money as a tool can be applied to finance,breaking the cycle of poverty and embracing a path towards collective prosperity
+
+Register here:events.indigenomicsinstitute…#Indigenomics#Saskatoon11","Carol Anne Hilton, MBA@Hesquiaht9 Apr 2024","Indigenomics DESIGN-Indigenous Wealth Dynamics will explore how Indigenous perspectives on money as a tool can be applied to finance,breaking the cycle of poverty and embracing a path towards collective prosperity
+
+Register here:events.indigenomicsinstitute…#Indigenomics#Saskatoon",9 Apr 2024,11,,
+Don Richardson,@Don_Richardson,,"Don Richardson@Don_Richardson8 Apr 2024Ontario Working in Partnership with Northern First Nations to End Reliance on Diesel Fuel - Indigenous participation, leadership, and support for energy projects is a key feature of electricity procurement in ""Powering Ontario’s Growth Plan""#Indigenomicsnews.ontario.ca/en/release/1…1",Don Richardson@Don_Richardson8 Apr 2024,"Ontario Working in Partnership with Northern First Nations to End Reliance on Diesel Fuel - Indigenous participation, leadership, and support for energy projects is a key feature of electricity procurement in ""Powering Ontario’s Growth Plan""#Indigenomicsnews.ontario.ca/en/release/1…",8 Apr 2024,1,,
+"Carol Anne Hilton, MBA",@Hesquiaht,,"Carol Anne Hilton, MBA@Hesquiaht5 Apr 2024This one-day forum is an invaluable opportunity for Indigenous peoples, entrepreneurs, and leaders to connect, share insights, and be empowered in advancing Indigenous strength.
+
+Register here:indigenomics.swoogo.com/DESI…#Indigenomics#Saskatoon#Indigenouseconomy1","Carol Anne Hilton, MBA@Hesquiaht5 Apr 2024","This one-day forum is an invaluable opportunity for Indigenous peoples, entrepreneurs, and leaders to connect, share insights, and be empowered in advancing Indigenous strength.
+
+Register here:indigenomics.swoogo.com/DESI…#Indigenomics#Saskatoon#Indigenouseconomy",5 Apr 2024,1,,
+Cultural Survival,@CSORG,,"Cultural Survival@CSORG5 Apr 2024Read about how Indigenous activists envision the soon-to-be Global Indigenous Technology House, an initiative to recognize Indigenous perspectives as foundational to the storytelling and technological advancements in the digital age.#Indigenomicsglobalcentreofindigenomics.c…1",Cultural Survival@CSORG5 Apr 2024,"Read about how Indigenous activists envision the soon-to-be Global Indigenous Technology House, an initiative to recognize Indigenous perspectives as foundational to the storytelling and technological advancements in the digital age.#Indigenomicsglobalcentreofindigenomics.c…",5 Apr 2024,1,,
+Don Richardson,@Don_Richardson,,"Don Richardson@Don_Richardson5 Apr 2024Raven Indigenous Capital Partners@Raven_Capitalgears up for busy year with first time founders: “It’s really thrilling to play a small, but catalytic role at Raven in revitalizing the Indigenous economy and leading some of the work around economic reconciliation""#Indigenomics1",Don Richardson@Don_Richardson5 Apr 2024,"Raven Indigenous Capital Partners@Raven_Capitalgears up for busy year with first time founders: “It’s really thrilling to play a small, but catalytic role at Raven in revitalizing the Indigenous economy and leading some of the work around economic reconciliation""#Indigenomics",5 Apr 2024,1,,
+"Carol Anne Hilton, MBA",@Hesquiaht,,"Carol Anne Hilton, MBA@Hesquiaht4 Apr 2024The Indigenomics Institute welcomes Cenovus Energy@cenovusas a Bronze Sponsor for Indigenomics DESIGN-Indigenous Wealth Dynamics! 
+
+Don't miss out on the Early Bird Registration, available until April 4th!  
+Register here:indigenomics.swoogo.com/DESI…#Indigenomics#Saskatoon1","Carol Anne Hilton, MBA@Hesquiaht4 Apr 2024","The Indigenomics Institute welcomes Cenovus Energy@cenovusas a Bronze Sponsor for Indigenomics DESIGN-Indigenous Wealth Dynamics! 
+
+Don't miss out on the Early Bird Registration, available until April 4th!  
+Register here:indigenomics.swoogo.com/DESI…#Indigenomics#Saskatoon",4 Apr 2024,1,,
+Don Richardson,@Don_Richardson,,"Don Richardson@Don_Richardson4 Apr 2024BC Hydro launches call for private wind, solar power producers to feed grid - Successful bids will also need to have a minimum of 25 per cent First Nations ownership, with the aim of supporting “meaningful economic reconciliation.”globalnews.ca/news/10400506/…#Indigenomics1",Don Richardson@Don_Richardson4 Apr 2024,"BC Hydro launches call for private wind, solar power producers to feed grid - Successful bids will also need to have a minimum of 25 per cent First Nations ownership, with the aim of supporting “meaningful economic reconciliation.”globalnews.ca/news/10400506/…#Indigenomics",4 Apr 2024,1,,
+Don Richardson,@Don_Richardson,,"Don Richardson@Don_Richardson3 Apr 2024VIEWPOINT - Heather@ExnerPirotand@jessecmccormick: How Indigenous participation can help drive the new nuclear age - Industry is learning that Indigenous partnerships are assets, not hurdles, to moving projects forward /@TheHubCanada#Indigenomicsthehub.ca/2024-04-03/heather…3",Don Richardson@Don_Richardson3 Apr 2024,"VIEWPOINT - Heather@ExnerPirotand@jessecmccormick: How Indigenous participation can help drive the new nuclear age - Industry is learning that Indigenous partnerships are assets, not hurdles, to moving projects forward /@TheHubCanada#Indigenomicsthehub.ca/2024-04-03/heather…",3 Apr 2024,3,,
