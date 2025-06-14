064a5a742b50859d326ab52897dea49156a7566f
kIndigenomics <kwaterman@indigenomics.com>
2025-06-14 16:51:39 -0700

    Add files via upload

---
+++ Data Collection & Processing/Twitter Datasets/data/email-address-change.js
@@ -0,0 +1,21 @@
+window.YTD.email_address_change.part0 = [
+  {
+    "emailAddressChange" : {
+      "accountId" : "249829356",
+      "emailChange" : {
+        "changedAt" : "2011-02-09T21:39:41.000Z",
+        "changedTo" : "cawaaknis@shaw.ca"
+      }
+    }
+  },
+  {
+    "emailAddressChange" : {
+      "accountId" : "249829356",
+      "emailChange" : {
+        "changedAt" : "2011-03-20T19:22:48.000Z",
+        "changedFrom" : "cawaaknis@shaw.ca",
+        "changedTo" : "cawaaknis@me.com"
+      }
+    }
+  }
+]
---
+++ Data Collection & Processing/Twitter Datasets/data/key-registry.js
@@ -0,0 +1,84 @@
+window.YTD.key_registry.part0 = [
+  {
+    "keyRegistryData" : {
+      "userId" : "249829356",
+      "registeredDevices" : {
+        "deviceMetadataList" : [
+          {
+            "userAgent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2.1 Safari/605.1.15",
+            "registrationToken" : "631c6d586750ef825304bf4ebabe11c6",
+            "identityKey" : "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE4VyiFNTkK6QddKnWdUadm4flrC9o7/L7BQ1XS6u6jmO/rvPY8C71Qia4fcqgwHdjfOHOdtSHwIWfq2EHiEkL/g==",
+            "createdAt" : "2024-05-15T20:12:13.277Z",
+            "deviceId" : "15ec72cd-41e7-48bf-828e-10f8f90e203b"
+          },
+          {
+            "userAgent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15",
+            "registrationToken" : "2de4190489a66dc69b323d27587f87f3",
+            "identityKey" : "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEy8BnvPFKK0kExFad53FS/AbO2cvpg+rZVELOf7jBDcc2svdHh86rw1y4hDji8NJYib23vdQFRl1eUA4wuDvDuA==",
+            "createdAt" : "2024-05-21T20:01:26.875Z",
+            "deviceId" : "26e3e5d8-2ddc-4898-b83b-3dbb4beae7bc"
+          },
+          {
+            "userAgent" : "TwitterAndroid/10.11.0-release.0 (310110000-r-0) TECNO+KH6/12 (TECNO;TECNO+KH6;TECNO;KH6-OP;0;;1;2015)",
+            "registrationToken" : "0252e77ede6f2ebb3dc630cb6cf5eae7",
+            "identityKey" : "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEfICPXZQwzkOs67CdBUHBlmaFmrd5d8KbEtL84OCCVQISg0/EWSjO/ioIDa5FaXMFNyQKZoE7elLedAPrB/++NA==",
+            "createdAt" : "2023-10-16T14:32:53.877Z",
+            "deviceId" : "276b0b10-5a35-4a55-b7fa-7f080408648f"
+          },
+          {
+            "userAgent" : "Twitter-iPhone/9.56.1 iOS/16.0.1 (Apple;iPhone14,7;;;;;1;2021)",
+            "registrationToken" : "5537f784efca1b7bc57ced8137f6caa8",
+            "identityKey" : "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEAkppl2r1Wuj6+OEFRUVCBT1TaRZ+uu1nBx9rcRsSAlwY53Bz9XtqpIVjFQhnE0ea4QXcJbgIF23pVwMLzPQB0g==",
+            "createdAt" : "2023-05-09T22:39:09.480Z",
+            "deviceId" : "52137606-A3E7-4304-A8ED-A669328F5554"
+          },
+          {
+            "userAgent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6.1 Safari/605.1.15",
+            "registrationToken" : "f83d127a4a9749fb2d207cc16d104977",
+            "identityKey" : "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEzr4IcPFzWYiZ6KARvC3F+5CUNtRdBJSNalvfoxQYlj2LYBB6V8vrD0PuuynhJlao3CIhmO/85VKDWzslWNmQzQ==",
+            "createdAt" : "2023-05-10T03:52:52.783Z",
+            "deviceId" : "5314615f-1721-4f8e-b08d-447339fdb1c3"
+          },
+          {
+            "userAgent" : "Twitter-iPad/10.16 iOS/17.1.1 (Apple;iPad13,16;;;;;1;2020)",
+            "registrationToken" : "cd8545954a654eee0bab604d22cfc73c",
+            "identityKey" : "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEqJWyjV45TtW2x5AoetwR+K6nhV9IHDJhe8DelLGpi6gDE4cYCJGTSLVGufC+jum1WauIx3gHJkkF84ZIKDgQBw==",
+            "createdAt" : "2023-11-22T14:16:03.197Z",
+            "deviceId" : "ADDB3CD3-FF91-4575-A3AB-6B54AD266A41"
+          },
+          {
+            "userAgent" : "Twitter-iPad/10.12 iOS/16.6 (Apple;iPad13,19;;;;;1;2020)",
+            "registrationToken" : "553989959334bc788709ea9ed57fa570",
+            "identityKey" : "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEu7EDycVsxBYF+bq5dbVN+PQekl+nn6RKE9nmxdCldJhH2moh3tF2zVivkR85iNDDvlWFFpVvZXaz93IjHjMdmA==",
+            "createdAt" : "2023-10-26T16:55:05.085Z",
+            "deviceId" : "F38302E9-5234-451F-95BF-707AC2992739"
+          },
+          {
+            "userAgent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15",
+            "registrationToken" : "395a08fb0eb7330f4863397bf74cb60d",
+            "identityKey" : "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEcuOY117BFA0vxLzJWfRiVpStpYMOodVWSuWVIEe50R9K/F7yJUF0hDadeWdywa2TWtG/A2KNB25fsAsxjvI0/A==",
+            "createdAt" : "2024-05-22T03:42:27.585Z",
+            "deviceId" : "abe1f0f9-1b43-4afb-9f4d-d2ba3b02bf24"
+          },
+          {
+            "userAgent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+            "registrationToken" : "2fb706483500f098300b4dd9397551d3",
+            "identityKey" : "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEqpdar7CuQpPIqTEWsujOL9hSGPqzE1kER+9D6KOMZGKyzaMZBdcAgPaWN5SAKXTZCXEER5DAiU8rv00uJjgm9A==",
+            "createdAt" : "2023-08-18T16:22:57.483Z",
+            "deviceId" : "eb6b91df-2fab-4c1e-8821-a38617133f19"
+          },
+          {
+            "userAgent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2.1 Safari/605.1.15",
+            "registrationToken" : "1cb3369c64001025cc5921442a159744",
+            "identityKey" : "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEgRb0BaaGnQQRT8Ed/yR2d8kCXkh1Yy0yBvJiLm2+SUyesZJC3xzWyT/X1WZIZD21AcJjZraO/e2teCRbPwPJ5g==",
+            "createdAt" : "2024-04-27T18:22:05.169Z",
+            "deviceId" : "fea618a5-5a97-44aa-b8c6-1e84b51353c5"
+          }
+        ]
+      },
+      "deregisteredDevices" : {
+        "deRegisteredDeviceMetadataList" : [ ]
+      }
+    }
+  }
+]
