4759eed522e7dbd1e57251e40891ad483d6e8c1c
kIndigenomics <kwaterman@indigenomics.com>
2025-06-14 16:49:37 -0700

    Add files via upload

---
+++ Data Collection & Processing/Twitter Datasets/data/sso.js
@@ -0,0 +1,10 @@
+window.YTD.sso.part0 = [
+  {
+    "singleSignOnMetaValues" : {
+      "ssoId" : "001856.2539e1b8b71944e4a3c4e1e7a655d6c2.1856",
+      "ssoEmail" : "cawaaknis@me.com",
+      "associationMethodType" : "Login",
+      "createdAt" : "2023-03-23T18:56:34.782Z"
+    }
+  }
+]
