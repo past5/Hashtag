20a6694dd1286314948a6ccdbfc15b115670296a
kIndigenomics <kwaterman@indigenomics.com>
2025-06-14 16:50:16 -0700

    Add files via upload

---
+++ Data Collection & Processing/Twitter Datasets/data/saved-search.js
@@ -0,0 +1,14 @@
+window.YTD.saved_search.part0 = [
+  {
+    "savedSearch" : {
+      "savedSearchId" : "98638617",
+      "query" : "#futureofmoney"
+    }
+  },
+  {
+    "savedSearch" : {
+      "savedSearchId" : "337381483",
+      "query" : "#natcap"
+    }
+  }
+]
